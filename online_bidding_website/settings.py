71122374b9c264173b7553825a6a6c33e703c0ec
obiyang <124426464+obiyang@users.noreply.github.com>
2024-01-10 20:53:22 -0800

    Update settings.py

--- online_bidding_website/settings.py
+++ online_bidding_website/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-#y@cphs@1c7i!fyp*s&(9r-t1-ybwc7a^su1$duw&3wn5bpj=m"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
